8fc0013213d4d90b43f6574f81254dd701da10a3
ChrisToffee <chris.s.dennis@gmail.com>
2014-08-20 17:34:56 +0100

    started refactoring and formatting
    started refactoring and formatting

--- SillyGooses.js
+++ SillyGooses.js
@@ -1,117 +1,170 @@
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
+/*global window */
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame       ||
           window.webkitRequestAnimationFrame ||
           window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
+          function(callback) { window.setTimeout(callback, 1000 / 60); };
+}());
 
-(function animloop(){
-  requestAnimFrame(animloop);
-  render();
-})();
-
-function render() {
-    if(context){
-      context.clearRect(0,0,canvasWidth,canvasHeight);
-      drawBackground();
-      drawPond();
-      drawSillyGooses();
-      rescuedGeese();
-      drawFox();
-      drawDuck();
-      gameText();
-      if(menu){
-        showMenu();
-      }
-    }
-}
-
-var canvas, context,toggle, togglePul = true,grassSize,
-  spaceDown = false,
-  menu = true,
-  pulsate = 0,
-  gooseLives = 3,
-  gooseMove = 0,
-  goosePosition = [],
-  duckOut = 70,
-  mousePosX = 100,
-  mousePosY = 100,
-  rescues = 0,
-  gooseRescues = 0,
-  gooseRescuesMove = 0,
-  laserBeam = false,
-  foxMove = 1,
-  foxPosition = [],
-  breadCrumbsArr = [],
-  lastPosArr = [100,100],
-  grassArr = [],
-  gooseArr = []
-  gooseRange = [[50,350,100,550],[50,950,100,150],[630,950,100,550],[50,950,450,550]];
+var canvas, context, toggle, grassSize, canvasXCenter, canvasYCenter,
+    togglePul = true,
+    spaceDown = false,
+    menu = true,
+    pulsate = 0,
+    gooseLives = 3,
+    gooseMove = 0,
+    goosePosition = [],
+    SillyGooseMaxPosition = [450, 450, 450, 350, 350, 270, 270, 270, 270, 270, 270, 270, 450, 450, 450, 450, 450, 450, 270, 270, 210, 210, 210, 210, 210, 210, 210, 350, 450, 450],
+    duckOut = 70,
+    rescues = 0,
+    gooseRescues = 0,
+    gooseRescuesMove = 0,
+    successRate = 0,
+    laserBeam = false,
+    foxMove = 3,
+    foxPosition = [],
+    breadCrumbsArr = [],
+    lastPosArr = [100,100],
+    grassArr = [],
+    gooseArr = [],
+    segements = [],
+    motherGHasBaby = false;
 
 function init() {
-   canvasWidth = 1000; 
-   canvasHeight = 600;
+    canvasWidth = 1000; 
+    canvasHeight = 600;
+    canvas = document.createElement( 'canvas' );
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
-   createBackgound();
-   createSillyGooses();
+    context = canvas.getContext( '2d' );
+    context.fillStyle = "rgb(117,153,90)";
+    context.rect(0, 0, canvasWidth, canvasHeight);
+    context.fill();
 
-   canvas = document.createElement( 'canvas' );
-   canvas.width = canvasWidth;
-   canvas.height = canvasHeight;
+    document.body.appendChild( canvas );
 
-   
     window.addEventListener( "keydown", doKeyDown, true);
     window.addEventListener( "keyup", doKeyUp, true);
 
-   canvasXCenter = canvasWidth/ 2;
-   canvasyCenter = canvasHeight / 2;
+    canvasXCenter = canvasWidth / 2;
+    canvasYCenter = canvasHeight / 2;
 
-   context = canvas.getContext( '2d' );
-   context.fillStyle = "rgb(117,153,90)";
-   context.rect(0, 0, canvasWidth, canvasHeight);
-   context.fill();
+    createBackgound();
+    createSillyGooses();
+    getSegements();
+}
 
-   document.body.appendChild( canvas );
+function getSegements () {
+    for (var i = 0; i <= 29; i++) {
+        var radians = (i / 15 * Math.PI),
+            segment = [];
+
+        segment.push(radians, false);
+        segements.push(segment); 
+    };
+}
+
+function drawSegment () {
+    var x = 0,
+        y = 0,
+        w = 200,
+        h = 1;
+
+    for (var arr in segements) {
+        for (var i = 0; i < segements[arr].length; i++) {
+            var r = segements[arr][0];
+
+            context.save();
+            context.translate(canvasXCenter, canvasYCenter);
+            context.rotate(r);
+            context.fillStyle = "rgb(252,238,33)";
+            context.beginPath();
+            context.fillRect(x,y, w, h);
+            context.closePath();
+            context.fill();
+            context.restore();
+        }
+    }
 }
 
 function createBackgound () {
-   for (i = 0; i <= 2000; i++) {
-      var grass = [];
-      // Get random positions for grasss.
-      var x = Math.floor(Math.random() * (canvasWidth - 1))
-      var y = Math.floor(Math.random() * (canvasHeight - 1))
-      toggle = !toggle;
+    for (i = 0; i <= 2000; i++) {
+        var grass = [],
+            x = Math.floor(Math.random() * (canvasWidth - 1)),
+            y = Math.floor(Math.random() * (canvasHeight - 1));
 
-      grassSize = toggle ? 3 : 5;
-      grassColour = toggle ? 'rgb(0,104,55)' :  'rgb(100,204,55)';
+        toggle = !toggle;
+        grassSize = toggle ? 3 : 5;
+        grassColour = toggle ? 'rgb(0,104,55)' : 'rgb(100,204,55)';
 
-      grass.push(x,y,grassSize,grassColour);
-
-      grassArr.push(grass);
-   }
+        grass.push(x,y,grassSize,grassColour);
+        grassArr.push(grass);
+    }
 }
 
 function createSillyGooses () {
+    for (var i = 0; i < 6; i++) {
+        var gooses = [],
+            randomSeg = getRandomInt(0,29),
+            x = getRandomGooseInt(200,SillyGooseMaxPosition[randomSeg]);
 
-  for (var arr in gooseRange) {
-      for (var i = 0; i < 3; i++) {
-         var gooses = [];
-        // Get random positions for grasss.
-        //var x = Math.floor(Math.random() * (canvasWidth - canvasWidth / 10));
-        var x = getRandomInt(gooseRange[arr][0],gooseRange[arr][1]);
-        var y = getRandomInt(gooseRange[arr][2],gooseRange[arr][3]);
-        var r = getRandomInt(0,360);
-
-        gooses.push(x,y,r,false);
+        gooses.push(x,randomSeg);
         gooseArr.push(gooses);
-      }
-   }
+    }
+}
+
+function drawSillyGooses () {
+    var y = -5,
+        w = 20,
+        h = 15;
+
+    for (var i = 0; i < gooseArr.length; i++) {
+        var r = segements[ gooseArr[i][1] ][0],
+            x = gooseArr[i][0];
+
+        context.save();
+        context.translate(canvasXCenter, canvasYCenter);
+        context.rotate(r);
+        context.fillStyle = "rgb(252,238,33)";
+        context.beginPath();
+        context.fillRect(x,y, w, h);
+        context.fillRect(x+3,y-5, w-6, h+10);
+        context.fillStyle = "rgb(222,208,33)";
+        context.fillRect(x+6,y-12, w-12, h);
+        context.fillStyle = "rgb(252,128,33)";
+        context.fillRect(x+8,y-16, w-16, h-6);
+        context.closePath();
+        context.fill();
+        context.restore();
+    }
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    if(gooseArr[0]){
+      do{
+        var exists = false,
+          number = Math.floor(Math.random() * (max - min + 1)) + min;
+        for (var i = 0; i < gooseArr.length; i++) {
+          if(number == gooseArr[i][1]){
+            exists = true;
+            break;
+          }
+        }
+      }while(exists);
+    }else{
+      var number = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return number;
+}
+
+function getRandomGooseInt(min, max) {
+
+    var range = max - min + 1;
+      range = range / 5,
+      number = Math.floor(Math.random() * (range));
+    
+    return (number * 5) + min;
 }
 
 function drawBackground() {
@@ -133,8 +186,9 @@ function drawBackground() {
 
 function drawFox () {
 
-  var cx = (Math.sin( foxMove )) + canvasWidth / 2,
-    cy = (Math.cos( foxMove ))+ canvasHeight / 2,
+//TODO:hit detection 
+  var cx = (Math.sin( foxMove )) + canvasXCenter,
+    cy = (Math.cos( foxMove ))+ canvasYCenter,
     x = 150,
     y = 0,
     r = -(foxMove),
@@ -166,10 +220,14 @@ function drawFox () {
   context.closePath();
   context.restore();
   if(!menu){
-    foxMove+=rescues/112;
+    foxMove+=(rescues+1)/100;
     gooseRescuesMove+=0.01;
+    if(Math.floor((foxMove / Math.PI)* 15) == 29){
+      foxMove = 0;
+    }
   }
-  
+  var csd = Math.floor((foxMove / Math.PI)* 15);
+  console.log('fox on segment',30-csd);
 }
 
 function doKeyDown (e) {
@@ -177,13 +235,19 @@ function doKeyDown (e) {
   //Left Arrow
   case 37:
     if(!spaceDown && duckOut <= 80){
-      gooseMove -= 0.08;
+      gooseMove -= 1;
+      if(gooseMove === -1){
+        gooseMove = 29;
+      }
     }
     break;
   //right arrow
   case 39:
     if(!spaceDown && duckOut <= 80){
-      gooseMove += 0.08;
+      gooseMove += 1;
+      if(gooseMove === 30){
+        gooseMove = 0;
+      }
     }
     break;
   //space bar
@@ -192,9 +256,8 @@ function doKeyDown (e) {
       menu = false;
     }
     if(!menu && duckOut <= 80){
-      spaceDown = true;
-      rescues++;
       gooseRescues++;
+      spaceDown = true;
     }
     break;
   case 80:
@@ -217,23 +280,35 @@ function doKeyUp (e) {
 }
 
 function drawDuck () {
+  if(!spaceDown && duckOut <= 80){
+      if(motherGHasBaby){
+        rescues++;
+        motherGHasBaby= false;
+      }
+    }
+  
   if(!spaceDown && duckOut > 70){
     duckOut-= 5;
   }
 
+  if(motherGHasBaby){
+    spaceDown=false;
+  }
+
   if(spaceDown && duckOut < 500){
+    
     duckOut+= 5;
   }
 
   var w = 20,
     h = 40,
-    cx = (Math.sin( gooseMove) ) + canvasWidth / 2,
-    cy = (Math.cos( gooseMove) ) + canvasHeight / 2,
+    cx = canvasXCenter,
+    cy = canvasYCenter,
     x = duckOut,
-    y = -20,
-    r = gooseMove;
+    y = -18,
+    r = (gooseMove / 15 * Math.PI);
 
-    goosePosition = [x,cy];
+    goosePosition = [gooseMove,x];
   context.save();
 
   context.fillStyle = "rgb(253,253,253)";
@@ -269,34 +344,34 @@ function drawDuck () {
   context.fillRect(x+45, y+17, w-10, h-34);
 
   context.restore();
-  console.log(goosePosition);
+  checkGooseHit();
 }
 
 
-function drawPond (argument) {
+function drawPond () {
   context.save();
   context.beginPath();
-  context.arc(canvasWidth/2, canvasHeight/2, 113, 0, 2 * Math.PI, false);
+  context.arc(canvasXCenter, canvasYCenter, 113, 0, 2 * Math.PI, false);
   context.fillStyle = 'rgba(73,56,49,0.3)';
   context.fill();
 
   context.beginPath();
-  context.arc(canvasWidth/2, canvasHeight/2, 110, 0, 2 * Math.PI, false);
+  context.arc(canvasXCenter, canvasYCenter, 110, 0, 2 * Math.PI, false);
   context.fillStyle = 'rgba(73,56,49,0.5)';
   context.fill();
 
   context.beginPath();
-  context.arc(canvasWidth/2, canvasHeight/2, 80 + pulsate, 0, 2 * Math.PI, false);
+  context.arc(canvasXCenter, canvasYCenter, 80 + pulsate, 0, 2 * Math.PI, false);
   context.fillStyle = 'rgba(0,126,128,0.5)';
   context.fill();
   
   context.beginPath();
-  context.arc(canvasWidth/2, canvasHeight/2, 90 + pulsate, 0, 2 * Math.PI, false);
+  context.arc(canvasXCenter, canvasYCenter, 90 + pulsate, 0, 2 * Math.PI, false);
   context.fillStyle = 'rgba(0,126,128,0.5)';
   context.fill();
 
   context.beginPath();
-  context.arc(canvasWidth/2, canvasHeight/2, 104 - pulsate, 0, 2 * Math.PI, false);
+  context.arc(canvasXCenter, canvasYCenter, 104 - pulsate, 0, 2 * Math.PI, false);
   context.fillStyle = 'rgba(0,126,128,0.5)';
   context.fill();
 context.restore();
@@ -317,39 +392,7 @@ context.restore();
   }
 }
 
-function drawSillyGooses (argument) {
 
-   for (var arr in gooseArr) {
-      for (var i = 0; i < gooseArr[arr].length; i++) {
-        var cx = gooseArr[arr][0],
-          cy = gooseArr[arr][1],
-          r = gooseArr[arr][2],
-          x = 0,
-          y = 0,
-          w = 20,
-          h = 15;
-
-          context.save();
-          context.translate(cx, cy);
-          context.rotate(r-(pulsate*2) * Math.PI/180);
-          context.fillStyle = "rgb(252,238,33)";
-          context.beginPath();
-          context.fillRect(x,y, w, h);
-          context.fillRect(x+3,y-5, w-6, h+10);
-          context.fillStyle = "rgb(222,208,33)";
-          context.fillRect(x+6,y-12, w-12, h);
-
-          context.fillStyle = "rgb(252,128,33)";
-          context.fillRect(x+8,y-16, w-16, h-6);
-
-          context.closePath();
-          context.fill();
-          context.restore();
-
-
-      }
-   }
-}
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return [evt.clientX - rect.left, evt.clientY - rect.top];
@@ -367,21 +410,21 @@ function showMenu () {
   context.fillStyle = "rgb(253,203,0)";
   context.textAlign = 'center';
   context.font="40px Calibri";
-  context.fillText("How to play",canvasWidth/2,150);
+  context.fillText("How to play",canvasXCenter,150);
   context.fillStyle = "rgb(239,218,171)";
   context.font="20px Calibri";
-  context.fillText("The baby geese have left the safety of the pond to go and explore",canvasWidth/2,180);
-  context.fillText("and have failed to realise the hungry fox looking for lunch!",canvasWidth/2,210);
-  context.fillText("...Silly gooses!",canvasWidth/2,240);
-  context.fillText("Help mother goose get her baby geese back to the safety of the pond.",canvasWidth/2,270);
-  context.fillText("Use the left and right arrow keys to navigate mother goose.",canvasWidth/2,330);
-  context.fillText("Then use the SPACEBAR to launch a rescue.",canvasWidth/2,360);
-  context.fillText("Press the SPACEBAR to start and P for pause.",canvasWidth/2,390);
-  context.fillText("...and L for laser beam vision... for acuracy.",canvasWidth/2,420);
+  context.fillText("The baby geese have left the safety of the pond to go and explore",canvasXCenter,180);
+  context.fillText("and have failed to realise the hungry fox looking for lunch!",canvasXCenter,210);
+  context.fillText("...Silly gooses!",canvasXCenter,240);
+  context.fillText("Help mother goose get her baby geese back to the safety of the pond.",canvasXCenter,270);
+  context.fillText("Use the left and right arrow keys to navigate mother goose.",canvasXCenter,330);
+  context.fillText("Then use the SPACEBAR to launch a rescue.",canvasXCenter,360);
+  context.fillText("Press the SPACEBAR to start and P for pause.",canvasXCenter,390);
+  context.fillText("...and L for laser beam vision... for acuracy.",canvasXCenter,420);
 
   context.fillStyle = "rgb(253,203,0)";
   context.font="15px Calibri";
-  context.fillText("Note: Yes I know its 'geese' and not 'gooses'. I just wanted a 'Silly Goose' game.",canvasWidth/2,450);
+  context.fillText("Note: Yes I know its 'geese' and not 'gooses'. I just wanted a 'Silly Goose' game.",canvasXCenter,450);
 
   context.restore();
 }
@@ -390,7 +433,7 @@ function rescuedGeese () {
   
 
     for (var i = 0; i < rescues; i++) {
-      var cx = (Math.sin( gooseRescuesMove + i )) + canvasWidth / 2,
+      var cx = (Math.sin( gooseRescuesMove + i )) + canvasXCenter,
         cy = (Math.cos( gooseRescuesMove + i ))+ canvasHeight / 2,
         x = 45,
         y = i*3,
@@ -417,10 +460,25 @@ function rescuedGeese () {
 }
 
 function checkGooseHit () {
-  // body...
+  //check to see if has baby geese
+  if(goosePosition[0]){
+    for (var i = 0; i < gooseArr.length; i++) {
+      if(goosePosition[0] == gooseArr[i][1]){
+        if (goosePosition[1] == gooseArr[i][0]) {
+          gooseArr.splice(i,1);
+          motherGHasBaby = true;
+        }
+        break;
+      }
+    }
+  }
+  //check if fox has lunch
+
 }
 
 function gameText () {
+  successRate = Math.ceil((rescues/gooseRescues) *100) || 0;
+
   context.save();
   context.fillStyle = "rgba(73,56,49,0.5)";
   context.beginPath();
@@ -440,8 +498,8 @@ function gameText () {
 
   context.font="20px Calibri";
   context.fillStyle = "rgb(239,218,171)";
-  context.fillText("Rescues Launch: " + rescues ,250,45);
-  context.fillText("Success Rate: " + gooseLives ,650,45);
+  context.fillText("Rescues Launch: " + gooseRescues ,250,45);
+  context.fillText("Success Rate: " + successRate + "%",650,45);
   context.fillStyle = "rgb(239,218,0)";
   context.fillText("Gooses Rescued: " + rescues ,450,45);
   context.fillText("Lives Left: " + gooseLives ,850,45);
@@ -454,4 +512,27 @@ function gameText () {
   context.restore();
 }
 
-      
+function render() {
+    if (context) {
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        drawBackground();
+        drawPond();
+        drawSillyGooses();
+        rescuedGeese();
+        drawFox();
+        drawDuck();
+        gameText();
+        if(menu) {
+            showMenu();
+        }
+        if(!gooseArr[0]) {
+          menu = true;
+        }
+        drawSegment();
+    }
+}
+
+(function animloop() {
+  requestAnimFrame(animloop);
+    render();
+}());      
